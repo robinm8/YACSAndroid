f5772f448d0d25f9e328673fcc685a4b0e7edf93
Mark Robinson <robinm8@keybase.io>
2016-09-24 02:03:47 -0400

    Merge upstream with local project
    Create initial application structure

---
+++ app/src/androidTest/java/edu/rpi/cs/yacs/ApplicationTest.java
@@ -0,0 +1,16 @@
+package edu.rpi.cs.yacs;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ *
+ */
+
+@SuppressWarnings("deprecation")
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
