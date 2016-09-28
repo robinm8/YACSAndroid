878b7ade837caff631bf73f2ee2f8207d6a2b991
Mark Robinson <robinm8@keybase.io>
2016-09-28 02:27:00 -0400

    Create persistent SettingsActivity Create prefs.xml with settings Modify styles to include settings Modify strings to include settings

---
+++ app/src/main/java/edu/rpi/cs/yacs/core/SettingsActivity.java
@@ -0,0 +1,72 @@
+package edu.rpi.cs.yacs.core;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
+import android.view.View;
+
+import com.afollestad.materialdialogs.MaterialDialog;
+
+import edu.rpi.cs.yacs.R;
+
+/**
+ * Created by Mark Robinson on 9/25/16.
+ */
+
+public class SettingsActivity extends PreferenceActivity {
+
+    public static class SettingsFragment extends PreferenceFragment {
+
+        @Override
+        public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, final Preference preference) {
+            if (preference.getKey().equals(getString(R.string.houseOfYACS))) {
+                new MaterialDialog.Builder(getActivity())
+                        .title(R.string.houseOfYACS)
+                        .items(R.array.home_YACS_values)
+                        .itemsCallbackSingleChoice(0, new MaterialDialog.ListCallbackSingleChoice() {
+                            @Override
+                            public boolean onSelection(MaterialDialog dialog, View itemView, int which, CharSequence text) {
+                                preference.setSummary(text);
+                                preference.setDefaultValue(text);
+                                return true;
+                            }
+                        })
+                        .positiveText(R.string.Select)
+                        .show();
+            }
+
+            return super.onPreferenceTreeClick(preferenceScreen, preference);
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            addPreferencesFromResource(R.xml.prefs);
+
+            SharedPreferences sp = getPreferenceScreen().getSharedPreferences();
+
+            Preference houseYACS = findPreference("House of YACS");
+            houseYACS.setSummary(sp.getString(getString(R.string.houseOfYACS), "Rensselaer Polytechnic Institute"));
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(android.R.id.content, new SettingsFragment())
+                    .commit();
+        }
+    }
+
+    @Override
+    protected boolean isValidFragment(String fragmentName) {
+        return SettingsFragment.class.getName().equals(fragmentName);
+    }
+}
