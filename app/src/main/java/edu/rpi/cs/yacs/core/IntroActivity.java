ec2515a25bbd320afe065c19eb5e8739a2c07a69
Mark Robinson <robinm8@keybase.io>
2016-10-07 07:59:47 -0400

    Disable launchMain() on SelectHomeSlide
    Disable only if the current slide is SelectHomeSlide and the House of YACS
    setting has not been set yet.
    
    This is to prevent the user from being able to skip to the main activity
    without first setting the YACS API base url.
    
    This is required in order to ensure YACS Android consistency.

--- app/src/main/java/edu/rpi/cs/yacs/core/IntroActivity.java
+++ app/src/main/java/edu/rpi/cs/yacs/core/IntroActivity.java
@@ -2,8 +2,10 @@ package edu.rpi.cs.yacs.core;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 
 import com.github.paolorotolo.appintro.AppIntro2;
@@ -18,19 +20,23 @@ import edu.rpi.cs.yacs.slides.SelectHomeSlide;
 
 public class IntroActivity extends AppIntro2 {
 
+    SelectHomeSlide selectHomeSlide = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        selectHomeSlide = new SelectHomeSlide();
+
         addSlide(AppIntroFragment.newInstance("Welcome To YACS!", "Short description \n", R.drawable.indicator_dot_white, Color.parseColor("#ef5350")));
         addSlide(AppIntroFragment.newInstance("YACS", "Description of features\n", R.drawable.indicator_dot_white, Color.parseColor("#ef5350")));
         addSlide(AppIntroFragment.newInstance("Google Calendar", "If you want, Google Calendar can be used to save your schedules.\n", R.drawable.indicator_dot_white, Color.parseColor("#ef5350")));
-        addSlide(new SelectHomeSlide());
+        addSlide(selectHomeSlide);
         addSlide(AppIntroFragment.newInstance("All Set!", "You're awesome, enjoy our app! \n", R.drawable.indicator_dot_white, Color.parseColor("#ef5350")));
 
-        askForPermissions(new String[]{Manifest.permission.READ_CALENDAR, Manifest.permission.WRITE_CALENDAR}, 2);
-
         setFlowAnimation();
+
+        askForPermissions(new String[]{Manifest.permission.READ_CALENDAR, Manifest.permission.WRITE_CALENDAR}, 3);
         setSwipeLock(false);
         setNextPageSwipeLock(false);
         setImmersiveMode(true);
@@ -38,16 +44,24 @@ public class IntroActivity extends AppIntro2 {
 
     @Override
     public void onSkipPressed(Fragment currentFragment) {
-        super.onSkipPressed(currentFragment);
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        String houseOfYACS = preferences.getString(getString(R.string.houseOfYACS), "unkown");
 
-        launchMain();
+        if (!currentFragment.getClass().equals(selectHomeSlide.getClass()) && !houseOfYACS.equals("unkown")) {
+            super.onSkipPressed(currentFragment);
+            launchMain();
+        }
     }
 
     @Override
     public void onDonePressed(Fragment currentFragment) {
-        super.onDonePressed(currentFragment);
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        String houseOfYACS = preferences.getString(getString(R.string.houseOfYACS), "unkown");
 
-        launchMain();
+        if (!currentFragment.getClass().equals(selectHomeSlide.getClass()) && !houseOfYACS.equals("unkown")) {
+            super.onDonePressed(currentFragment);
+            launchMain();
+        }
     }
 
     public void launchMain() {
