e044613c15426d32f9428097cc6b105097cfb53f
Mark Robinson <robinm8@keybase.io>
2016-10-11 00:06:01 -0400

    Create YACSApplication Singleton Instance

--- app/src/main/java/edu/rpi/cs/yacs/core/YACSApplication.java
+++ app/src/main/java/edu/rpi/cs/yacs/core/YACSApplication.java
@@ -8,5 +8,23 @@ import edu.rpi.cs.yacs.retrofit.ServiceHelper;
  * Created by Mark Robinson on 10/4/16.
  */
 public class YACSApplication extends Application {
-    ServiceHelper serviceHelper = new ServiceHelper(getApplicationContext());
+    private static YACSApplication instance;
+
+    private ServiceHelper serviceHelper = null;
+
+    public static YACSApplication getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+
+        serviceHelper = new ServiceHelper(getApplicationContext());
+    }
+
+    public ServiceHelper getServiceHelper() {
+        return serviceHelper;
+    }
 }
