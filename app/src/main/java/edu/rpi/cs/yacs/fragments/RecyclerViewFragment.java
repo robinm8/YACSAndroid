f4b968402e3712a68a5fc42568d0c81ef5b9181b
Mark Robinson <robinm8@keybase.io>
2016-10-16 03:36:28 -0400

    Modify RecyclerViewFragment, Load Departments
    This commit is the one that brings YACS departments view to YACS Android!

--- app/src/main/java/edu/rpi/cs/yacs/fragments/RecyclerViewFragment.java
+++ app/src/main/java/edu/rpi/cs/yacs/fragments/RecyclerViewFragment.java
@@ -1,21 +1,37 @@
 package edu.rpi.cs.yacs.fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.OvershootInterpolator;
 
 import com.github.florent37.materialviewpager.MaterialViewPagerHelper;
 import com.github.florent37.materialviewpager.adapter.RecyclerViewMaterialAdapter;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import edu.rpi.cs.yacs.R;
+import edu.rpi.cs.yacs.adapters.SchoolsAdapter;
+import edu.rpi.cs.yacs.core.YACSApplication;
+import edu.rpi.cs.yacs.models.School;
+import edu.rpi.cs.yacs.models.Schools;
+import jp.wasabeef.recyclerview.adapters.AlphaInAnimationAdapter;
+import jp.wasabeef.recyclerview.adapters.ScaleInAnimationAdapter;
+import jp.wasabeef.recyclerview.animators.SlideInUpAnimator;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by Mark Robinson on 9/24/16.
@@ -24,10 +40,11 @@ import edu.rpi.cs.yacs.R;
 public class RecyclerViewFragment extends Fragment {
 
     private static final String ARG_TAB_TITLE = "tabTitle";
-    private static final int ITEM_COUNT = 5;
 
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
+    private SchoolsAdapter schoolsAdapter;
+    private AlphaInAnimationAdapter alphaInAnimationAdapter;
     private List<Object> mContentItems = new ArrayList<>();
     private String tabTitle;
 
@@ -59,20 +76,73 @@ public class RecyclerViewFragment extends Fragment {
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
         mRecyclerView.setLayoutManager(layoutManager);
         mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setItemAnimator(new SlideInUpAnimator());
 
         switch (tabTitle) {
             case "Explore":
-                mAdapter = new RecyclerViewMaterialAdapter(new ExploreRecyclerViewAdapter(mContentItems));
-                mRecyclerView.setAdapter(mAdapter);
+                Call<Schools> schoolCall = YACSApplication.getInstance().getServiceHelper().getService().loadSchools();
 
-                mContentItems.add(new Object());
+                schoolCall.enqueue(new Callback<Schools>() {
+                    @Override
+                    public void onResponse(Call<Schools> call, Response<Schools> response) {
+                        List<School> schoolList = null;
+
+                        if (response.isSuccessful()) {
+                            Schools schools = response.body();
+
+                            schoolList = schools.getSchools();
+                        } else {
+                            int statusCode = response.code();
+
+                            ResponseBody errorBody = response.errorBody();
+
+                            getActivity().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.youtube.com/watch?v=t3otBjVZzT0")));
+
+                            Log.d("Retrofit Call", "Server error " + statusCode);
+
+                            try {
+                                Log.d("Retrofit Call", errorBody.string());
+                            } catch (IOException ignored) {
+                            }
+                        }
+
+                        createSchoolsAdapter(schoolList);
+                    }
+
+                    @Override
+                    public void onFailure(Call<Schools> call, Throwable t) {
+                        Log.d("Retrofit Call", "Server failed :(");
+
+                        List<School> list = new ArrayList<>();
+                        createSchoolsAdapter(list);
+
+                        getActivity().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.youtube.com/watch?v=t3otBjVZzT0")));
+                    }
+                });
+
                 break;
             case "Schedule":
-                mAdapter = new RecyclerViewMaterialAdapter(new ScheduleRecyclerViewAdapter(mContentItems));
-                mRecyclerView.setAdapter(mAdapter);
-                mContentItems.add(new Object());
+                //TODO create schedule adapter
+
+                List<School> list = new ArrayList<>();
+                createSchoolsAdapter(list);
+
                 break;
         }
+    }
+
+    public void createSchoolsAdapter(List<School> schoolList) {
+        schoolsAdapter = new SchoolsAdapter(schoolList);
+        mAdapter = new RecyclerViewMaterialAdapter(schoolsAdapter);
+
+        alphaInAnimationAdapter = new AlphaInAnimationAdapter(mAdapter);
+        alphaInAnimationAdapter.setDuration(1000);
+        alphaInAnimationAdapter.setFirstOnly(false);
+        alphaInAnimationAdapter.setInterpolator(new OvershootInterpolator());
+
+        mRecyclerView.setAdapter(new ScaleInAnimationAdapter(alphaInAnimationAdapter));
+
+        mContentItems.add(new Object());
 
         mAdapter.notifyDataSetChanged();
         MaterialViewPagerHelper.registerRecyclerView(getActivity(), mRecyclerView);
