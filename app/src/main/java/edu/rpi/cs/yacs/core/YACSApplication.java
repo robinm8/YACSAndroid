05aabbe790f36197e00a53dbf377e3002965b95f
Mark Robinson <robinm8@keybase.io>
2016-10-16 03:41:20 -0400

    Create RealmDB Object In YACSApplication
    This commit creates the foundation for a persistent cache to
    leverage API server resources.

--- app/src/main/java/edu/rpi/cs/yacs/core/YACSApplication.java
+++ app/src/main/java/edu/rpi/cs/yacs/core/YACSApplication.java
@@ -3,6 +3,8 @@ package edu.rpi.cs.yacs.core;
 import android.app.Application;
 
 import edu.rpi.cs.yacs.retrofit.ServiceHelper;
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
 
 /**
  * Created by Mark Robinson on 10/4/16.
@@ -11,6 +13,7 @@ public class YACSApplication extends Application {
     private static YACSApplication instance;
 
     private ServiceHelper serviceHelper = null;
+    private Realm realm = null;
 
     public static YACSApplication getInstance() {
         return instance;
@@ -22,9 +25,18 @@ public class YACSApplication extends Application {
         instance = this;
 
         serviceHelper = new ServiceHelper(getApplicationContext());
+
+        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder(this).build();
+        Realm.setDefaultConfiguration(realmConfiguration);
+
+        realm = Realm.getDefaultInstance();
     }
 
     public ServiceHelper getServiceHelper() {
         return serviceHelper;
+    }
+
+    public Realm getRealm() {
+        return realm;
     }
 }
