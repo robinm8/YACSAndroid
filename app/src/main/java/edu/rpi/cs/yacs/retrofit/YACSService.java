563fc8264779923d479e66ae0bbda87ab8b5b02e
Mark Robinson <robinm8@keybase.io>
2016-10-15 20:58:04 -0400

    Use New Models In YACSService
    This commit modifies the YACSService to use the new models.

--- app/src/main/java/edu/rpi/cs/yacs/retrofit/YACSService.java
+++ app/src/main/java/edu/rpi/cs/yacs/retrofit/YACSService.java
@@ -1,8 +1,8 @@
 package edu.rpi.cs.yacs.retrofit;
 
-import edu.rpi.cs.yacs.models.Course;
-import edu.rpi.cs.yacs.models.School;
-import edu.rpi.cs.yacs.models.Section;
+import edu.rpi.cs.yacs.models.Courses;
+import edu.rpi.cs.yacs.models.Schools;
+import edu.rpi.cs.yacs.models.Sections;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
@@ -16,8 +16,8 @@ public interface YACSService {
      * This method fetches all schools and departments of the college.
      * @return Relevant School objects
      */
-    @GET("schools.json?show_departments=true")
-    Call<School> loadSchools();
+    @GET("schools.json?show_departments")
+    Call<Schools> loadSchools();
 
     /**
      * This method fetches all courses given a query to search.
@@ -25,7 +25,7 @@ public interface YACSService {
      * @return Relevant Course objects of query
      */
     @GET("courses.json")
-    Call<Course> loadCoursesBySearch(@Query("search") String search);
+    Call<Courses> loadCoursesBySearch(@Query("search") String search);
 
     /**
      * This method fetches all courses of a given department.
@@ -33,7 +33,7 @@ public interface YACSService {
      * @return Relevant Course objects of department_code
      */
     @GET("courses.json")
-    Call<Course> loadCoursesByDepartment(@Query("department_code") String department_code);
+    Call<Courses> loadCoursesByDepartment(@Query("department_code") String department_code);
 
     /**
      * This method fetches all sections and periods of a given course.
@@ -41,5 +41,5 @@ public interface YACSService {
      * @return Relevant Section objects of course_id
      */
     @GET("sections.json?show_periods=true")
-    Call<Section> loadCourseSections(@Query("course_id") int course_id);
+    Call<Sections> loadCourseSections(@Query("course_id") int course_id);
 }
