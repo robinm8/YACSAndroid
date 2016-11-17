3213314697a8c35e90f964589bfd9e8649daf3f7
Mark Robinson <robinm8@keybase.io>
2016-11-17 00:19:26 -0500

    Refactor ItemViewHolder

---
+++ app/src/main/java/edu/rpi/cs/yacs/viewholders/SchoolItemViewHolder.java
@@ -0,0 +1,59 @@
+package edu.rpi.cs.yacs.viewholders;
+
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.ViewPropertyAnimatorListener;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import edu.rpi.cs.yacs.R;
+import jp.wasabeef.recyclerview.animators.holder.AnimateViewHolder;
+
+/**
+ * Created by Mark Robinson on 10/15/16.
+ */
+public class SchoolItemViewHolder extends AnimateViewHolder {
+    @BindView(R.id.name)
+    TextView nameView;
+
+    @BindView(R.id.code)
+    TextView codeView;
+
+    public SchoolItemViewHolder(View itemView) {
+        super(itemView);
+        ButterKnife.bind(this, itemView);
+    }
+
+    @Override
+    public void animateRemoveImpl(ViewPropertyAnimatorListener listener) {
+        ViewCompat.animate(itemView)
+                .translationY(-itemView.getHeight() * 0.3f)
+                .alpha(0)
+                .setDuration(300)
+                .setListener(listener)
+                .start();
+    }
+
+    @Override
+    public void preAnimateAddImpl() {
+        ViewCompat.setTranslationY(itemView, -itemView.getHeight() * 0.3f);
+        ViewCompat.setAlpha(itemView, 0);
+    }
+
+    @Override
+    public void animateAddImpl(ViewPropertyAnimatorListener listener) {
+        ViewCompat.animate(itemView)
+                .translationY(0)
+                .alpha(1)
+                .setDuration(300)
+                .setListener(listener)
+                .start();
+    }
+
+    public void render(String name, String code){
+        nameView.setText(name);
+        codeView.setText(code);
+    }
+}
