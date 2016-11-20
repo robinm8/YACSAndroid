1f687960c42baa6b803554ecc0ad9ef6f6fc4fe3
Mark Robinson <robinm8@keybase.io>
2016-11-19 20:28:25 -0500

    Create on Failed Snackbar
    This snackbar will give the user an option to retry connection to YACS server if YACS server connection ever timed out.

--- app/src/main/java/edu/rpi/cs/yacs/fragments/RecyclerViewFragment.java
+++ app/src/main/java/edu/rpi/cs/yacs/fragments/RecyclerViewFragment.java
@@ -1,9 +1,9 @@
 package edu.rpi.cs.yacs.fragments;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -25,6 +25,7 @@ import edu.rpi.cs.yacs.adapters.SchoolsAdapter;
 import edu.rpi.cs.yacs.core.YACSApplication;
 import edu.rpi.cs.yacs.models.School;
 import edu.rpi.cs.yacs.models.Schools;
+import edu.rpi.cs.yacs.retrofit.YACSService;
 import jp.wasabeef.recyclerview.adapters.AlphaInAnimationAdapter;
 import jp.wasabeef.recyclerview.adapters.ScaleInAnimationAdapter;
 import jp.wasabeef.recyclerview.animators.SlideInUpAnimator;
@@ -41,24 +42,28 @@ public class RecyclerViewFragment extends Fragment {
 
     private static final String ARG_TAB_TITLE = "tabTitle";
 
+    CoordinatorLayout coordinatorLayout = null;
+
     private RecyclerView mRecyclerView;
 
     public RecyclerView.Adapter getMAdapter() {
         return mAdapter;
     }
 
-    private RecyclerView.Adapter mAdapter;
-    private SchoolsAdapter schoolsAdapter;
-    private AlphaInAnimationAdapter alphaInAnimationAdapter;
-    private ScaleInAnimationAdapter scaleInAnimationAdapter;
+    private RecyclerView.Adapter mAdapter = null;
+    private SchoolsAdapter schoolsAdapter = null;
+    private AlphaInAnimationAdapter alphaInAnimationAdapter = null;
+    private ScaleInAnimationAdapter scaleInAnimationAdapter = null;
     private List<Object> mContentItems = new ArrayList<>();
     private String tabTitle;
+    private YACSService webService = null;
 
     public static RecyclerViewFragment newInstance(String tabTitle) {
         RecyclerViewFragment fragment = new RecyclerViewFragment();
         Bundle args = new Bundle();
         args.putString(ARG_TAB_TITLE, tabTitle);
         fragment.setArguments(args);
+
         return fragment;
     }
 
@@ -68,6 +73,8 @@ public class RecyclerViewFragment extends Fragment {
         if (getArguments() != null) {
             tabTitle = getArguments().getString(ARG_TAB_TITLE);
         }
+
+        webService = YACSApplication.getInstance().getServiceHelper().getService();
     }
 
     @Override
@@ -78,6 +85,7 @@ public class RecyclerViewFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        coordinatorLayout = (CoordinatorLayout) view.findViewById(R.id.coordinatorLayout);
         mRecyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
         mRecyclerView.setLayoutManager(layoutManager);
@@ -86,57 +94,58 @@ public class RecyclerViewFragment extends Fragment {
 
         switch (tabTitle) {
             case "Explore":
-                Call<Schools> schoolCall = YACSApplication.getInstance().getServiceHelper().getService().loadSchools();
-
-                schoolCall.enqueue(new Callback<Schools>() {
-                    @Override
-                    public void onResponse(Call<Schools> call, Response<Schools> response) {
-                        List<School> schoolList = null;
-
-                        if (response.isSuccessful()) {
-                            Schools schools = response.body();
-
-                            schoolList = schools.getSchools();
-                        } else {
-                            int statusCode = response.code();
-
-                            ResponseBody errorBody = response.errorBody();
-
-                            getActivity().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.youtube.com/watch?v=t3otBjVZzT0")));
-
-                            Log.d("Retrofit Call", "Server error " + statusCode);
-
-                            try {
-                                Log.d("Retrofit Call", errorBody.string());
-                            } catch (IOException ignored) {
-                            }
-                        }
-
-                        createSchoolsAdapter(schoolList);
-                    }
-
-                    @Override
-                    public void onFailure(Call<Schools> call, Throwable t) {
-                        Log.d("Retrofit Call", "Failed. Retrying...");
-
-                        List<School> list = new ArrayList<>();
-                        createSchoolsAdapter(list);
-
-                        call.clone();
-
-                        // TODO: Replace with a "pull down to refresh" layout, soon
-                    }
-                });
+                populateSchoolsAdapter();
 
                 break;
             case "Schedule":
-                //TODO create schedule adapter
+                //TODO: create schedule adapter
 
                 List<School> list = new ArrayList<>();
                 createSchoolsAdapter(list);
 
                 break;
         }
+    }
+
+    public void populateSchoolsAdapter() {
+        Call<Schools> schoolCall = webService.loadSchools();
+
+        schoolCall.enqueue(new Callback<Schools>() {
+            @Override
+            public void onResponse(Call<Schools> call, Response<Schools> response) {
+                List<School> schoolList = null;
+
+                if (response.isSuccessful()) {
+                    Schools schools = response.body();
+
+                    schoolList = schools.getSchools();
+                } else {
+                    int statusCode = response.code();
+
+                    ResponseBody errorBody = response.errorBody();
+
+                    Log.d("Retrofit Call", "Server error " + statusCode);
+
+                    try {
+                        Log.d("Retrofit Call Error", errorBody.string());
+                    } catch (IOException ignored) {}
+
+                    createFailedSnackbar();
+                }
+
+                createSchoolsAdapter(schoolList);
+            }
+
+            @Override
+            public void onFailure(Call<Schools> call, Throwable t) {
+                Log.d("Retrofit Call", "Failed.");
+
+                List<School> list = new ArrayList<>();
+                createSchoolsAdapter(list);
+
+                createFailedSnackbar();
+            }
+        });
     }
 
     public void createSchoolsAdapter(List<School> schoolList) {
@@ -157,5 +166,23 @@ public class RecyclerViewFragment extends Fragment {
 
         mAdapter.notifyDataSetChanged();
         MaterialViewPagerHelper.registerRecyclerView(getActivity(), mRecyclerView);
+    }
+
+    public void createFailedSnackbar() {
+        Snackbar snackbar = Snackbar
+                .make(coordinatorLayout, "Connection timed out.", Snackbar.LENGTH_INDEFINITE)
+                .setAction("RETRY", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        view.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                populateSchoolsAdapter();
+                            }
+                        }, 250);
+                    }
+                });
+
+        snackbar.show();
     }
 }
