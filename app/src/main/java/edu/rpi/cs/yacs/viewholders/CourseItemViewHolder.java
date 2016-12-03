f6997424ab7c2e7bf89b2df7abf9232eec8e2025
Mark Robinson <robinm8@keybase.io>
2016-12-03 16:44:58 -0500

    Modify CourseItemViewHolder

--- app/src/main/java/edu/rpi/cs/yacs/viewholders/CourseItemViewHolder.java
+++ app/src/main/java/edu/rpi/cs/yacs/viewholders/CourseItemViewHolder.java
@@ -1,5 +1,7 @@
 package edu.rpi.cs.yacs.viewholders;
 
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.ViewPropertyAnimatorListener;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
@@ -11,11 +13,12 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import edu.rpi.cs.yacs.R;
 import edu.rpi.cs.yacs.models.Section;
+import jp.wasabeef.recyclerview.animators.holder.AnimateViewHolder;
 
 /**
  * Created by Mark Robinson on 11/17/16.
  */
-public class CourseItemViewHolder extends RecyclerView.ViewHolder{
+public class CourseItemViewHolder extends AnimateViewHolder {
     @BindView(R.id.name)
     TextView nameView;
 
@@ -34,6 +37,32 @@ public class CourseItemViewHolder extends RecyclerView.ViewHolder{
     public CourseItemViewHolder(View itemView) {
         super(itemView);
         ButterKnife.bind(this, itemView);
+    }
+
+    @Override
+    public void animateRemoveImpl(ViewPropertyAnimatorListener listener) {
+        ViewCompat.animate(itemView)
+                .translationY(-itemView.getHeight() * 0.3f)
+                .alpha(0)
+                .setDuration(300)
+                .setListener(listener)
+                .start();
+    }
+
+    @Override
+    public void preAnimateAddImpl() {
+        ViewCompat.setTranslationY(itemView, -itemView.getHeight() * 0.3f);
+        ViewCompat.setAlpha(itemView, 0);
+    }
+
+    @Override
+    public void animateAddImpl(ViewPropertyAnimatorListener listener) {
+        ViewCompat.animate(itemView)
+                .translationY(0)
+                .alpha(1)
+                .setDuration(300)
+                .setListener(listener)
+                .start();
     }
 
     public void render(String name, String code, String credits, String description, List<Section> sections){
