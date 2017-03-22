5e9a340fe34c252090902e941c367852b224e9ef
Mark Robinson <robinm8@rpi.edu>
2017-03-22 02:15:02 -0400

    Create ViewPagerMode.java
    Makes enum-like Strings

---
+++ app/src/main/java/edu/rpi/cs/yacs/enums/ViewPagerMode.java
@@ -0,0 +1,12 @@
+package edu.rpi.cs.yacs.enums;
+
+/**
+ * Created by mark on 3/23/17.
+ */
+
+public final class ViewPagerMode {
+    public static final String EXPLORE = "Explore";
+    public static final String SCHEDULE = "Schedule";
+
+    private ViewPagerMode() { }
+}
